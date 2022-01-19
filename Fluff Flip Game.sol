51b6e344b95183a8d856d346663bf8b66d86f2f8
fluffinu <98059950+fluffinu@users.noreply.github.com>
2022-01-19 14:00:53 -0800

    Create Fluff Flip Game.sol

---
+++ Fluff Flip Game.sol
@@ -0,0 +1,38 @@
+contract FluffFlip {
+
+  enum GameState {noWager, wagerMade, wagerAccepted}
+  GameState public currentState;
+  uint public wager;
+  address public player1;
+  address public player2;
+
+  modifier onlyState(GameState expectedState) { if(expectedState == currentState) { _; } else { throw; } }
+
+  function FluffFlip() {
+    currentState = GameState.noWager;
+  }
+
+  function makeWager() onlyState(GameState.noWager) payable returns (bool) {
+    wager = msg.value; // eth value that gets sent into contract
+    player1 = msg.sender; // address of whoever is calling contract
+    currentState = GameState.wagerMade;
+    return true;
+  }
+
+  function acceptWager() onlyState(GameState.wagerMade) payable returns (bool) {
+    if (msg.value == wager) {
+      player2 = msg.sender;
+      currentState = GameState.wagerAccepted;
+      return true;
+    } else {
+      throw;
+    }
+  }
+
+  function resolveBet() onlyState(GameState.wagerAccepted) returns (bool) {
+    // ...
+    currentState = GameState.noWager;
+    return true;
+  }
+
+}
